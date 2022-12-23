bfe7fd38eb8ae638e096d742cbc4dbe6bcc64565
adrianrezha29 <adrianrezha29@gmail.com>
2022-12-23 15:46:23 +0700

    Add project files.

---
+++ EXE5_060_Kelompok7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE5_060_Kelompok7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
